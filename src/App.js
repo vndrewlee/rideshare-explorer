1eb5ee4f83ee4595fa3140b72cbe106a7c52f7de
vndrewlee <hello@vndrewlee.com>
2021-01-16 10:30:22 -0500

    add link back to blog

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-import VegaLite from 'react-vega-lite';
-import {Handler} from 'vega-tooltip';
+import VegaLite from "react-vega-lite";
+import { Handler } from "vega-tooltip";
 
-import {Range} from 'rc-slider';
-import 'rc-slider/assets/index.css';
+import { Range } from "rc-slider";
+import "rc-slider/assets/index.css";
 
-import {timeFormat} from "d3-time-format";
+import { timeFormat } from "d3-time-format";
 
-import timeseries from './data.js';
-import ResolutionInput from './ResolutionInput';
-import ResultMap from './ResultMap';
-import {ridesSpec, distanceSpec} from './vegaliteSpecs';
-
+import timeseries from "./data.js";
+import ResolutionInput from "./ResolutionInput";
+import ResultMap from "./ResultMap";
+import { ridesSpec, distanceSpec } from "./vegaliteSpecs";
 
 function App() {
   const [resolution, setResolution] = useState("week");
@@ -26,37 +25,43 @@ function App() {
   const [max, setMax] = useState(1486857600000);
   const [displayMin, setDisplayMin] = useState(1465014900000);
   const [displayMax, setDisplayMax] = useState(1486857600000);
-  const [extent, ] = useState([1465014900000, 1486857600000]);
+  const [extent] = useState([1465014900000, 1486857600000]);
 
   useEffect(() => {
-    setData(timeseries[resolution])
+    setData(timeseries[resolution]);
   }, [resolution]);
 
   useEffect(() => {
-    const newFilteredData = data.filter(datum => datum.timestamp >= min && datum.timestamp <= max);
+    const newFilteredData = data.filter(
+      (datum) => datum.timestamp >= min && datum.timestamp <= max
+    );
     setFilteredData(newFilteredData);
   }, [min, max, data]);
 
   return (
     <Container>
-      <br/>
-      <hr/>
+      <br />
+      <hr />
       <h1>Ride Share Austin</h1>
       <p>
-        A dataset of ~900K ride share trips in the city of Austin, TX from May 2016 to Feb 2017.
+        A dataset of ~900K ride share trips in the city of Austin, TX from May
+        2016 to Feb 2017.
       </p>
+      <a href={"https://www.vndrewlee.com/rideshareexplorer/"}>
+        Project Details
+      </a>
       <a href={"https://data.world/andytryba/rideaustin"}>Data Source</a>
-      <hr/>
+      <hr />
       <Row>
         <Col sm={12} lg={6}>
           <h3>Date Range</h3>
           <Row>
-            <Col><p align={"left"}>
-              Min: {formatTime(new Date(displayMin))}
-            </p></Col>
-            <Col><p align={"right"}>
-              Max: {formatTime(new Date(displayMax))}
-            </p></Col>
+            <Col>
+              <p align={"left"}>Min: {formatTime(new Date(displayMin))}</p>
+            </Col>
+            <Col>
+              <p align={"right"}>Max: {formatTime(new Date(displayMax))}</p>
+            </Col>
           </Row>
           <Range
             min={extent[0]}
@@ -74,31 +79,33 @@ function App() {
         </Col>
         <Col sm={12} lg={6}>
           <h3>Resolution</h3>
-          <ResolutionInput
-            selection={resolution}
-            callBack={setResolution}
-          />
+          <ResolutionInput selection={resolution} callBack={setResolution} />
         </Col>
       </Row>
-      <hr/>
+      <hr />
       <Row>
         <Col sm={12} lg={6}>
           <h3>Rides Completed</h3>
-          <VegaLite spec={ridesSpec} data={{values: filteredData}} tooltip={new Handler().call}/>
+          <VegaLite
+            spec={ridesSpec}
+            data={{ values: filteredData }}
+            tooltip={new Handler().call}
+          />
         </Col>
         <Col sm={12} lg={6}>
           <h3>Kilometers Driven</h3>
-          <VegaLite spec={distanceSpec} data={{values: filteredData}} tooltip={new Handler().call}/>
+          <VegaLite
+            spec={distanceSpec}
+            data={{ values: filteredData }}
+            tooltip={new Handler().call}
+          />
         </Col>
       </Row>
-      <br/>
+      <br />
       <h3>Dropoff Distribution</h3>
-      <ResultMap start={min} end={max}/>
-      <hr/>
-      <p>
-        hello@vndrewlee.com
-      </p>
-
+      <ResultMap start={min} end={max} />
+      <hr />
+      <p>hello@vndrewlee.com</p>
     </Container>
   );
 }
